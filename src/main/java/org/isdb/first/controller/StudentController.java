ed2771d2d3c868cf974d16e5a3b198569b778d21
Farzana Akter <farzana.nilu99@gmail.com>
2025-03-22 10:39:30 +0600

    teacher

--- src/main/java/org/isdb/first/controller/StudentController.java
+++ src/main/java/org/isdb/first/controller/StudentController.java
@@ -4,16 +4,25 @@ import java.util.List;
 
 import org.isdb.first.model.Student;
 import org.isdb.first.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/student")
 public class StudentController {
 
+	@Value("${custom.env}")
+	private String env;
+
+	@Autowired
 	private final StudentService service;
 
 	// contractor injection
